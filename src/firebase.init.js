c6fa5cc2b0dafe04a38e8477ea1e02c038dfe021
sarkerrakib <freelancerrakib25@gmail.com>
2022-04-18 23:46:17 +0600

    readme file updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyAYSG3L7t0k1tXbbkqA9z8ueaoU6Elf10o",
+  authDomain: "jsr-photography.firebaseapp.com",
+  projectId: "jsr-photography",
+  storageBucket: "jsr-photography.appspot.com",
+  messagingSenderId: "378947709235",
+  appId: "1:378947709235:web:68eafee49348dea804f646"
 };
 
 // Initialize Firebase
